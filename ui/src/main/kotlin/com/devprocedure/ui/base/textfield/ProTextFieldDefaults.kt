819e9bcb0e2750070241b6817234529a0918b15a
emre.bahadir <emrebhdr16@gmail.com>
2023-10-06 22:32:19 +0300

    ProOutlinedTextField added

---
+++ ui/src/main/kotlin/com/devprocedure/ui/base/textfield/ProTextFieldDefaults.kt
@@ -0,0 +1,67 @@
+package com.devprocedure.ui.base.textfield
+
+import androidx.compose.foundation.text.selection.TextSelectionColors
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.runtime.Composable
+import com.composethemer.ProOutlinedTextFieldThemes
+import com.devprocedure.ui.base.textfield.prooutlinedtextfield.ProOutlinedTextFieldTheme
+
+/**
+ * Created by emre bahadir on 10/6/2023
+ */
+object ProTextFieldDefaults {
+    @Composable
+    internal fun outlinedTextFieldTheme() = ProOutlinedTextFieldThemes.Primary.theme
+
+    @Composable
+    internal fun outlinedTextFieldColors(
+        theme: ProOutlinedTextFieldTheme
+    ) = OutlinedTextFieldDefaults.colors(
+        focusedTextColor = theme.focusedTextColor().value,
+        unfocusedTextColor = theme.unfocusedTextColor().value,
+        disabledTextColor = theme.disabledTextColor().value,
+        errorTextColor = theme.errorTextColor().value,
+        focusedContainerColor = theme.focusedContainerColor().value,
+        unfocusedContainerColor = theme.unfocusedContainerColor().value,
+        disabledContainerColor = theme.disabledContainerColor().value,
+        errorContainerColor = theme.errorContainerColor().value,
+        cursorColor = theme.cursorColor().value,
+        errorCursorColor = theme.errorCursorColor().value,
+        selectionColors = TextSelectionColors(
+            handleColor = theme.selectionColorsHandleColor().value,
+            backgroundColor = theme.selectionColorsBackgroundColor().value
+        ),
+        focusedBorderColor = theme.focusedBorderColor().value,
+        unfocusedBorderColor = theme.unfocusedBorderColor().value,
+        disabledBorderColor = theme.disabledBorderColor().value,
+        errorBorderColor = theme.errorBorderColor().value,
+        focusedLeadingIconColor = theme.focusedLeadingIconColor().value,
+        unfocusedLeadingIconColor = theme.unfocusedLeadingIconColor().value,
+        disabledLeadingIconColor = theme.disabledLeadingIconColor().value,
+        errorLeadingIconColor = theme.errorLeadingIconColor().value,
+        focusedTrailingIconColor = theme.focusedTrailingIconColor().value,
+        unfocusedTrailingIconColor = theme.unfocusedTrailingIconColor().value,
+        disabledTrailingIconColor = theme.disabledTrailingIconColor().value,
+        errorTrailingIconColor = theme.errorTrailingIconColor().value,
+        focusedLabelColor = theme.focusedLabelColor().value,
+        unfocusedLabelColor = theme.unfocusedLabelColor().value,
+        disabledLabelColor = theme.disabledLabelColor().value,
+        errorLabelColor = theme.errorLabelColor().value,
+        focusedPlaceholderColor = theme.focusedPlaceholderColor().value,
+        unfocusedPlaceholderColor = theme.unfocusedPlaceholderColor().value,
+        disabledPlaceholderColor = theme.disabledPlaceholderColor().value,
+        errorPlaceholderColor = theme.errorPlaceholderColor().value,
+        focusedSupportingTextColor = theme.focusedSupportingTextColor().value,
+        unfocusedSupportingTextColor = theme.unfocusedSupportingTextColor().value,
+        disabledSupportingTextColor = theme.disabledSupportingTextColor().value,
+        errorSupportingTextColor = theme.errorSupportingTextColor().value,
+        focusedPrefixColor = theme.focusedPrefixColor().value,
+        unfocusedPrefixColor = theme.unfocusedPrefixColor().value,
+        disabledPrefixColor = theme.disabledPrefixColor().value,
+        errorPrefixColor = theme.errorPrefixColor().value,
+        focusedSuffixColor = theme.focusedSuffixColor().value,
+        unfocusedSuffixColor = theme.unfocusedSuffixColor().value,
+        disabledSuffixColor = theme.disabledSuffixColor().value,
+        errorSuffixColor = theme.errorSuffixColor().value
+    )
+}
---
+++ ui/src/main/kotlin/com/devprocedure/ui/base/textfield/prooutlinedtextfield/ProOutlinedTextField.kt
@@ -0,0 +1,105 @@
+package com.devprocedure.ui.base.textfield.prooutlinedtextfield
+
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.VisualTransformation
+import com.devprocedure.ui.base.icon.proicon.ProIcon
+import com.devprocedure.ui.base.text.ProText
+import com.devprocedure.ui.base.textfield.ProTextFieldDefaults
+import com.devprocedure.ui.util.ProImageVector
+
+/**
+ * Created by emre bahadir on 10/6/2023
+ */
+@Composable
+fun ProOutlinedTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    readOnly: Boolean = false,
+    label: String? = null,
+    placeholder: String? = null,
+    leadingIcon: ProImageVector? = null,
+    trailingIcon: ProImageVector? = null,
+    prefix: String? = null,
+    suffix: String? = null,
+    supportingText: String? = null,
+    isError: Boolean = false,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
+    singleLine: Boolean = true,
+    maxLines: Int = if (singleLine) 1 else Int.MAX_VALUE,
+    minLines: Int = 1,
+    theme: ProOutlinedTextFieldTheme = ProTextFieldDefaults.outlinedTextFieldTheme()
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        modifier = modifier,
+        enabled = enabled,
+        readOnly = readOnly,
+        textStyle = theme.textStyle().value,
+        label = label?.let {
+            {
+                ProText(
+                    text = label
+                )
+            }
+        },
+        placeholder = placeholder?.let {
+            {
+                ProText(
+                    text = placeholder
+                )
+            }
+        },
+        leadingIcon = leadingIcon?.let {
+            {
+                ProIcon(
+                    imageVector = leadingIcon
+                )
+            }
+        },
+        trailingIcon = trailingIcon?.let {
+            {
+                ProIcon(
+                    imageVector = trailingIcon
+                )
+            }
+        },
+        prefix = prefix?.let {
+            {
+                ProText(
+                    text = prefix
+                )
+            }
+        },
+        suffix = suffix?.let {
+            {
+                ProText(
+                    text = suffix
+                )
+            }
+        },
+        supportingText = supportingText?.let {
+            {
+                ProText(
+                    text = supportingText
+                )
+            }
+        },
+        isError = isError,
+        visualTransformation = visualTransformation,
+        keyboardOptions = keyboardOptions,
+        keyboardActions = keyboardActions,
+        singleLine = singleLine,
+        maxLines = maxLines,
+        minLines = minLines,
+        colors = ProTextFieldDefaults.outlinedTextFieldColors(theme)
+    )
+}
