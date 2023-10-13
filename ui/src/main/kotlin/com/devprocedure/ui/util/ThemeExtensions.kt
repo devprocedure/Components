be6e8d89edc438f038166a9ab6e31f64fff8b315
emre.bahadir <emrebhdr16@gmail.com>
2023-10-13 15:42:25 +0300

    ProCard catalogs added

--- ui/src/main/kotlin/com/devprocedure/ui/util/ThemeExtensions.kt
+++ ui/src/main/kotlin/com/devprocedure/ui/util/ThemeExtensions.kt
@@ -5,14 +5,14 @@ import kotlin.reflect.KClass
 /**
  * Created by emre bahadir on 10/13/2023
  */
-fun <Theme: Any>KClass<Theme>.getThemeList() = this.sealedSubclasses
+fun <Theme : Any> KClass<Theme>.getThemeList() = this.sealedSubclasses
     .map {
         it.objectInstance?.let {
             it::class.simpleName
         }
     }.mapNotNull { it }
 
-fun <Theme: Any>KClass<Theme>.getThemeByName(themeName: String): Theme {
+fun <Theme : Any> KClass<Theme>.getThemeByName(themeName: String): Theme {
     val theme = this.sealedSubclasses.find {
         it.objectInstance?.let { it::class.simpleName } == themeName
     }?.objectInstance
